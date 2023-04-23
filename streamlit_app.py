36d4e135fb3b435271752e1860572944674a150c
LiviaChen <66782449+LiviaChen@users.noreply.github.com>
2023-04-24 00:06:41 +0800

    hash the unwork part on streamlit cloud

--- streamlit_app.py
+++ streamlit_app.py
@@ -225,80 +225,80 @@ with c5 :
 
 # ---
 #from streamlit_elements import elements, dashboard, mui, editor, media, lazy, sync, nivo
-from pathlib import Path
-import json
+#from pathlib import Path
+#import json
 
-with st.sidebar :
-    media_url = st.text_input("Media URL", value="https://www.youtube.com/watch?v=ujVgw7qpZRQ")
-if "data" not in st.session_state:
-    st.session_state.data = Path("data.json").read_text()
+#with st.sidebar :
+#    media_url = st.text_input("Media URL", value="https://www.youtube.com/watch?v=ujVgw7qpZRQ")
+#if "data" not in st.session_state:
+#    st.session_state.data = Path("data.json").read_text()
 
-layout = [
-    dashboard.Item("editor", 0, 0, 6, 3),
-    dashboard.Item("chart", 6, 0, 6, 3),
-    dashboard.Item("media", 0, 2, 12, 4),
-]
+#layout = [
+#    dashboard.Item("editor", 0, 0, 6, 3),
+#    dashboard.Item("chart", 6, 0, 6, 3),
+#    dashboard.Item("media", 0, 2, 12, 4),
+#]
 
-with elements("demo") :
-    with dashboard.Grid(layout, draggableHandle=".draggable"):
-        with mui.Card(key="editor", sx={"display": "flex", "flexDirection": "column"}):
-            mui.CardHeader(title="Editor", className="draggable")
-            with mui.CardContent(sx={"flex": 1, "minHeight": 0}):
-                editor.Monaco(
-                    defaultValue=st.session_state.data,
-                    language="json",
-                    onChange=lazy(sync("data"))
-                )
-        with mui.CardActions:
-            mui.Button("Apply changes", onClick=sync())
-        with mui.Card(key="chart", sx={"display": "flex", "flexDirection": "column"}):
-            mui.CardHeader(title="Chart", className="draggable")
-            with mui.CardContent(sx={"flex": 1, "minHeight": 0}):
-                nivo.Bump(
-                    data=json.loads(st.session_state.data),
-                    colors={ "scheme": "spectral" },
-                    lineWidth=3,
-                    activeLineWidth=6,
-                    inactiveLineWidth=3,
-                    inactiveOpacity=0.15,
-                    pointSize=10,
-                    activePointSize=16,
-                    inactivePointSize=0,
-                    pointColor={ "theme": "background" },
-                    pointBorderWidth=3,
-                    activePointBorderWidth=3,
-                    pointBorderColor={ "from": "serie.color" },
-                    axisTop={
-                        "tickSize": 5,
-                        "tickPadding": 5,
-                        "tickRotation": 0,
-                        "legend": "",
-                        "legendPosition": "middle",
-                        "legendOffset": -36
-                    },
-                    axisBottom={
-                        "tickSize": 5,
-                        "tickPadding": 5,
-                        "tickRotation": 0,
-                        "legend": "",
-                        "legendPosition": "middle",
-                        "legendOffset": 32
-                    },
-                    axisLeft={
-                        "tickSize": 5,
-                        "tickPadding": 5,
-                        "tickRotation": 0,
-                        "legend": "ranking",
-                        "legendPosition": "middle",
-                        "legendOffset": -40
-                    },
-                    margin={ "top": 40, "right": 100, "bottom": 40, "left": 60 },
-                    axisRight=None,
-                )
-        with mui.Card(key="media", sx={"display": "flex", "flexDirection": "column"}):
-            mui.CardHeader(title="Media Player", className="draggable")
-            with mui.CardContent(sx={"flex": 1, "minHeight": 0}):
-                media.Player(url=media_url, width="100%", height="100%", controls=True)
+#with elements("demo") :
+#    with dashboard.Grid(layout, draggableHandle=".draggable"):
+#        with mui.Card(key="editor", sx={"display": "flex", "flexDirection": "column"}):
+#            mui.CardHeader(title="Editor", className="draggable")
+#            with mui.CardContent(sx={"flex": 1, "minHeight": 0}):
+#               editor.Monaco(
+#                    defaultValue=st.session_state.data,
+#                    language="json",
+#                    onChange=lazy(sync("data"))
+#                )
+#        with mui.CardActions:
+#            mui.Button("Apply changes", onClick=sync())
+#        with mui.Card(key="chart", sx={"display": "flex", "flexDirection": "column"}):
+#            mui.CardHeader(title="Chart", className="draggable")
+#            with mui.CardContent(sx={"flex": 1, "minHeight": 0}):
+#                nivo.Bump(
+#                    data=json.loads(st.session_state.data),
+#                   colors={ "scheme": "spectral" },
+#                    lineWidth=3,
+#                    activeLineWidth=6,
+#                    inactiveLineWidth=3,
+#                    inactiveOpacity=0.15,
+#                    pointSize=10,
+#                    activePointSize=16,
+#                    inactivePointSize=0,
+#                    pointColor={ "theme": "background" },
+#                    pointBorderWidth=3,
+#                    activePointBorderWidth=3,
+#                    pointBorderColor={ "from": "serie.color" },
+#                    axisTop={
+#                        "tickSize": 5,
+#                        "tickPadding": 5,
+#                        "tickRotation": 0,
+#                        "legend": "",
+#                        "legendPosition": "middle",
+#                        "legendOffset": -36
+#                    },
+#                    axisBottom={
+#                        "tickSize": 5,
+#                        "tickPadding": 5,
+#                        "tickRotation": 0,
+#                        "legend": "",
+#                        "legendPosition": "middle",
+#                        "legendOffset": 32
+#                    },
+#                    axisLeft={
+#                        "tickSize": 5,
+#                        "tickPadding": 5,
+#                        "tickRotation": 0,
+#                        "legend": "ranking",
+#                        "legendPosition": "middle",
+#                        "legendOffset": -40
+#                    },
+#                    margin={ "top": 40, "right": 100, "bottom": 40, "left": 60 },
+#                    axisRight=None,
+#                )
+#        with mui.Card(key="media", sx={"display": "flex", "flexDirection": "column"}):
+#            mui.CardHeader(title="Media Player", className="draggable")
+#            with mui.CardContent(sx={"flex": 1, "minHeight": 0}):
+#                media.Player(url=media_url, width="100%", height="100%", controls=True)
 
 # ---
 
