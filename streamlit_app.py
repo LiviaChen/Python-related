1c321277ef30aa92b17a0d42707690b4f103e89f
LiviaChen <66782449+LiviaChen@users.noreply.github.com>
2023-04-23 23:50:46 +0800

    streamlit app 30days final

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import altair as alt
+import requests
+
+st.set_page_config(layout="wide", page_title="LiviaZC 30days streamlit", page_icon="💐")
 
 # ---
 
@@ -122,7 +125,7 @@ else:
 my_bar.progress(100)
 
 # ----
-st.subheader('column combine sidebar objecrs practice')
+st.subheader('column combine sidebar objects practice')
 name = st.sidebar.text_input("what's your good name?")
 age = st.sidebar.slider('your good age?', 18, 60, 18)
 hobbie = st.sidebar.selectbox('your good hobbies', ('eat', 'cool', 'netflix'))
@@ -194,3 +197,327 @@ with col2 :
 st.write(st.session_state)
 
 # ---
+
+st.subheader('the way to use API')
+st.title('Bored API')
+selected_type = st.sidebar.selectbox('Select an activity type for API', ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"])
+suggested_activity_url = f'http://www.boredapi.com/api/activity?type={selected_type}'
+json_data = requests.get(suggested_activity_url)
+suggested_activity = json_data.json()
+
+c1, c2 = st.columns(2)
+with c1 :
+    with st.expander('about this part') :
+        st.write('this part suggest things you can do while you are bored based on the cate. you chose, powered by Bored API')
+with c2 :
+    st.write(suggested_activity)
+
+st.write('suggested activity')
+st.info(suggested_activity['activity'])
+
+c3, c4, c5 = st.columns(3)
+with c3 :
+    st.metric(label = 'number of people required', value = suggested_activity['participants'])
+with c4 :
+    st.metric(label = 'what type', value = suggested_activity['type'].capitalize())
+with c5 :
+    st.metric(label = 'link', value = suggested_activity['link'])
+
+# ---
+from streamlit_elements import elements, dashboard, mui, editor, media, lazy, sync, nivo
+from pathlib import Path
+import json
+
+with st.sidebar :
+    media_url = st.text_input("Media URL", value="https://www.youtube.com/watch?v=ujVgw7qpZRQ")
+if "data" not in st.session_state:
+    st.session_state.data = Path("data.json").read_text()
+
+layout = [
+    dashboard.Item("editor", 0, 0, 6, 3),
+    dashboard.Item("chart", 6, 0, 6, 3),
+    dashboard.Item("media", 0, 2, 12, 4),
+]
+
+with elements("demo") :
+    with dashboard.Grid(layout, draggableHandle=".draggable"):
+        with mui.Card(key="editor", sx={"display": "flex", "flexDirection": "column"}):
+            mui.CardHeader(title="Editor", className="draggable")
+            with mui.CardContent(sx={"flex": 1, "minHeight": 0}):
+                editor.Monaco(
+                    defaultValue=st.session_state.data,
+                    language="json",
+                    onChange=lazy(sync("data"))
+                )
+        with mui.CardActions:
+            mui.Button("Apply changes", onClick=sync())
+        with mui.Card(key="chart", sx={"display": "flex", "flexDirection": "column"}):
+            mui.CardHeader(title="Chart", className="draggable")
+            with mui.CardContent(sx={"flex": 1, "minHeight": 0}):
+                nivo.Bump(
+                    data=json.loads(st.session_state.data),
+                    colors={ "scheme": "spectral" },
+                    lineWidth=3,
+                    activeLineWidth=6,
+                    inactiveLineWidth=3,
+                    inactiveOpacity=0.15,
+                    pointSize=10,
+                    activePointSize=16,
+                    inactivePointSize=0,
+                    pointColor={ "theme": "background" },
+                    pointBorderWidth=3,
+                    activePointBorderWidth=3,
+                    pointBorderColor={ "from": "serie.color" },
+                    axisTop={
+                        "tickSize": 5,
+                        "tickPadding": 5,
+                        "tickRotation": 0,
+                        "legend": "",
+                        "legendPosition": "middle",
+                        "legendOffset": -36
+                    },
+                    axisBottom={
+                        "tickSize": 5,
+                        "tickPadding": 5,
+                        "tickRotation": 0,
+                        "legend": "",
+                        "legendPosition": "middle",
+                        "legendOffset": 32
+                    },
+                    axisLeft={
+                        "tickSize": 5,
+                        "tickPadding": 5,
+                        "tickRotation": 0,
+                        "legend": "ranking",
+                        "legendPosition": "middle",
+                        "legendOffset": -40
+                    },
+                    margin={ "top": 40, "right": 100, "bottom": 40, "left": 60 },
+                    axisRight=None,
+                )
+        with mui.Card(key="media", sx={"display": "flex", "flexDirection": "column"}):
+            mui.CardHeader(title="Media Player", className="draggable")
+            with mui.CardContent(sx={"flex": 1, "minHeight": 0}):
+                media.Player(url=media_url, width="100%", height="100%", controls=True)
+
+# ---
+
+from streamlit_shap import st_shap
+import shap
+from sklearn.model_selection import train_test_split
+import xgboost
+
+@st.cache_data
+def load_data():
+    return shap.datasets.adult()
+
+@st.cache_data
+def load_model(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=7)
+    d_train = xgboost.DMatrix(X_train, label=y_train)
+    d_test = xgboost.DMatrix(X_test, label=y_test)
+    params = {
+        "eta": 0.01,
+        "objective": "binary:logistic",
+        "subsample": 0.5,
+        "base_score": np.mean(y_train),
+        "eval_metric": "logloss",
+        "n_jobs": -1,
+    }
+    model = xgboost.train(params, d_train, 10, evals = [(d_test, "test")], verbose_eval=100, early_stopping_rounds=20)
+    return model
+
+st.title("`streamlit-shap` displaying SHAP plots")
+
+st.subheader('Input data')
+X,y = load_data()
+X_display,y_display = shap.datasets.adult(display=True)
+
+with st.expander('About the data'):
+    st.write('Adult census data is used as the example dataset.')
+with st.expander('X'):
+    st.dataframe(X)
+with st.expander('y'):
+    st.dataframe(y)
+# with st.expander('X write'):  # the effect is same as above
+#     st.write(X)
+# with st.expander('y write'):
+#     st.write(y)
+
+st.subheader('SHAP output')
+
+# train XGBoost model
+model = load_model(X, y)
+
+# compute SHAP values
+explainer = shap.Explainer(model, X)
+shap_values = explainer(X)
+
+with st.expander('Waterfall plot'):
+    st_shap(shap.plots.waterfall(shap_values[0]), height=300)
+
+# with st.expander('Beeswarm plot'):
+#     st_shap(shap.plots.beeswarm(shap_values), height=300)
+
+explainer = shap.TreeExplainer(model)
+shap_values = explainer.shap_values(X)
+
+with st.expander('Force plot'):
+    st.subheader('First 1')
+    st_shap(shap.force_plot(explainer.expected_value, shap_values[0,:], X_display.iloc[0,:]), height=200, width=1000)
+    st.subheader('First 1000')
+    st_shap(shap.force_plot(explainer.expected_value, shap_values[:1000,:], X_display.iloc[:1000,:]), height=400, width=1000)
+
+# ---
+# hugging face
+# code can refer to https://github.com/charlywargnier/zero-shot-classifier/blob/main/streamlit_app.py
+
+from streamlit_option_menu import option_menu
+
+with st.sidebar:
+    selected = option_menu(
+        "",
+        ["Demo", "Unlocked Mode"],
+        icons=["bi-joystick", "bi-key-fill"],
+        menu_icon="",
+        default_index=0,
+    )
+
+API_KEY = st.secrets["hugface"]
+API_URL = (
+    "https://api-inference.huggingface.co/models/valhalla/distilbart-mnli-12-3"
+)
+headers = {"Authorization": f"Bearer {API_KEY}"}
+
+from streamlit_tags import st_tags, st_tags_sidebar
+label_widget = st_tags(
+    label="",
+    text="Add labels - 3 max",
+    value=["Transactional", "Informational"],
+    suggestions=[
+        "Navigational",
+        "Transactional",
+        "Informational",
+        "Positive",
+        "Negative",
+        "Neutral",
+    ],
+    maxtags=3,
+)
+MAX_LINES = 5
+
+sample = """
+I want to buy something in this store
+How to ask a question about a product
+Request a refund through the Google Play store
+I have a broken screen, what should I do?
+Can I have the link to the product?
+"""
+
+text = st.text_area(
+    "Enter keyphrase to classify",
+    sample,
+    height=200,
+    key="2",
+    help="At least two keyphrases for the classifier to work, one per line, "
+    + str(MAX_LINES)
+    + " keyphrases max as part of the demo",
+)
+lines = text.split("\n")  # A list of lines
+lines = list(filter(None, set(lines)))
+
+if len(lines) > MAX_LINES:
+    st.info(
+        f"🚨 Only the first "
+        + str(MAX_LINES)
+        + " keyprases will be reviewed. Unlock that limit by switching to 'Unlocked Mode'"
+    )
+
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    # Unhash to check status codes from the API response
+    # st.write(response.status_code)
+    return response.json()    
+
+listToAppend = []
+for row in lines:
+    output2 = query(
+                {
+                    "inputs": row,
+                    "parameters": {"candidate_labels": label_widget},
+                    "options": {"wait_for_model": True},
+                }
+            )
+    listToAppend.append(output2)
+
+df = pd.DataFrame.from_dict(listToAppend)
+
+from st_aggrid import AgGrid
+from st_aggrid.grid_options_builder import GridOptionsBuilder
+from st_aggrid.shared import JsCode
+from st_aggrid import GridUpdateMode, DataReturnMode
+
+gb = GridOptionsBuilder.from_dataframe(df)
+gb.configure_default_column(
+    enablePivot=True, enableValue=True, enableRowGroup=True
+)
+gb.configure_selection(selection_mode="multiple", use_checkbox=True)
+gb.configure_side_bar()  # side_bar is clearly a typo :) should by sidebar
+gridOptions = gb.build()
+
+response = AgGrid(
+    df,
+    gridOptions=gridOptions,
+    enable_enterprise_modules=True,
+    update_mode=GridUpdateMode.MODEL_CHANGED,
+    data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
+    height=400,
+    fit_columns_on_grid_load=False,
+    configure_side_bar=True,
+)
+    
+
+@st.cache_data
+def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv().encode("utf-8")
+
+csv = convert_df(df)
+
+st.caption("")
+
+st.download_button(
+    label="Download results as CSV",
+    data=csv,
+    file_name="results.csv",
+    mime="text/csv",
+)
+
+# end of hugging face
+# ---
+
+# youtube thumbnail
+st.sidebar.header('Settings')
+img_dict = {'Max': 'maxresdefault', 'High': 'hqdefault', 'Medium': 'mqdefault', 'Standard': 'sddefault'}
+selected_img_quality = st.sidebar.selectbox('Select image quality', ['Max', 'High', 'Medium', 'Standard'])
+img_quality = img_dict[selected_img_quality]
+
+yt_url = st.text_input('Paste YouTube URL', 'https://www.youtube.com/watch?v=ujVgw7qpZRQ')
+
+def get_ytid(input_url):
+  if 'youtu.be' in input_url:
+    ytid = input_url.split('/')[-1]
+  if 'youtube.com' in input_url:
+    ytid = input_url.split('=')[-1]
+  return ytid
+
+if yt_url != '':
+  ytid = get_ytid(yt_url) # yt or yt_url
+
+  yt_img = f'http://img.youtube.com/vi/{ytid}/{img_quality}.jpg'
+  st.image(yt_img)
+  st.write('YouTube video thumbnail image URL: ', yt_img)
+else:
+  st.write('☝️ Enter URL to continue ...')
+
+# done with 30 days challenge, yeahhhh
